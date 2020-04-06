21fe9f18e8714a87b026f72e0dc793c98cea2c84
artra <artra@DESKTOP-MHFIKDK>
2020-04-06 17:33:35 +0530

    Created angular project and completed feature 1

--- onlinebookstore/src/main/java/com/aritra/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/aritra/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.aritra.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.aritra.onlinebookstore.entity.Book;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
